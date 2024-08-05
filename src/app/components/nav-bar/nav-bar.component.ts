775641fb88315bc2ce748fb667e0cc70af0a4fe2
HugoBDev <hugobrd.pro@gmail.com>
2024-08-05 19:18:06 +0200

    working on csss for formula options and rating component

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { SubCardComponent } from '../sub-card/sub-card.component';
-import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-nav-bar',
